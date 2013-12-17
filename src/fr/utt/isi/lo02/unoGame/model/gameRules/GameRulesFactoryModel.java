b60483e51947ae7d4b61a2919f2d74e4d3e467cb
jeanmichelly <jeanmichely@gmail.com>
2013-12-17 13:24:20 +0100

    Javadoc package gameRules

--- src/fr/utt/isi/lo02/unoGame/model/gameRules/GameRulesFactoryModel.java
+++ src/fr/utt/isi/lo02/unoGame/model/gameRules/GameRulesFactoryModel.java
@@ -2,13 +2,16 @@ package fr.utt.isi.lo02.unoGame.model.gameRules;
 
 import fr.utt.isi.lo02.unoGame.model.user.UserModel;
 
-/**
- * 
- * Cette classe utilise le patron fabrique pour permettre le choix dynamique du mode de jeu choisit par l'utilisateur.
- *
+/** 
+ * <u>Pattern Fabrique : </u> </br> 
+ * Permettre le choix dynamique du mode de jeu choisit par l'utilisateur.
  */
 public class GameRulesFactoryModel {
 
+    /**
+     * Creer le mode de jeu choisit par l'utilisateur
+     * @return le mode de jeu choisit par l'utilisateur
+     */
     public GameRulesModel createGameRules () {
         switch( UserModel.getChoiceGameRules() ) {
             case 's' :
